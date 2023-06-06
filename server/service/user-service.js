ba904315f74873cf191f172fa3bc73146c0c5f7f
volkovich <volkoviysash@gmail.com>
2023-06-07 01:58:33 +0300

    feat: Add users registration.

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,3 +1,27 @@
-class UserService {}
+const UserModel = require("../models/user-model");
+const bcrypt = require("bcrypt");
+const uuid = require("uuid");
+const mailService = require("./mail-service");
+
+class UserService {
+  async registration(email, password) {
+    const candidate = await UserModel.findOne({ email });
+
+    if (candidate) {
+      throw new Error(`User with email ${email} already exist.`);
+    }
+    // Hash the password
+    const hashPassword = await bcrypt.hash(password, 3);
+    // Generate the random string as activation link
+    const activationLink = uuid.v4;
+    const user = await UserModel.create({
+      email,
+      password: hashPassword,
+      activationLink,
+    });
+    // Send activation message to email
+    await mailService.sendActivationMail(email, activationLink);
+  }
+}
 
 module.exports = new UserService();
