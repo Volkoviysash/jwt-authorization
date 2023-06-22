4b1f67b5cd0d2a3d34c19b7459f9b1df88d2a000
volkovich <volkoviysash@gmail.com>
2023-06-23 02:39:11 +0300

    feat: Add interceptor for adding token to request.

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -7,3 +7,10 @@ const $api = axios.create({
   withCredentials: true,
   baseURL: API_URL,
 });
+
+$api.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+  return config;
+});
+
+export default $api;
