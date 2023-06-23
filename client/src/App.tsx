cc3009eb5d4c96ccf62e8b452848aec40ed29dee
volkovich <volkoviysash@gmail.com>
2023-06-23 13:38:03 +0300

    feat: Add Mobx and global state.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,13 @@
 import React, { FC } from "react";
 import "./App.css";
+import LoginForm from "./components/LoginForm";
 
 const App: FC = () => {
-  return <div className='App'>Hello!</div>;
+  return (
+    <div className='App'>
+      <LoginForm />
+    </div>
+  );
 };
 
 export default App;
