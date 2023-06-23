ea0cf7f27a5a952c0efea4be5b1fabcc687701ef
volkovich <volkoviysash@gmail.com>
2023-06-23 14:09:36 +0300

    feat: Check if user is authorized.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,62 @@
-import React, { FC } from "react";
+import React, { FC, useContext, useEffect, useState } from "react";
 import "./App.css";
 import LoginForm from "./components/LoginForm";
+import { Context } from "./index";
+import { observer } from "mobx-react-lite";
+import { IUser } from "./models/IUser";
+import UserService from "./services/UserService";
 
 const App: FC = () => {
+  const { store } = useContext(Context);
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      store.checkAuth();
+    }
+  }, []);
+
+  async function getUsers() {
+    try {
+      const response = await UserService.fetchUsers();
+      setUsers(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  if (store.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!store.isAuth) {
+    return (
+      <div>
+        <LoginForm />
+        <button onClick={getUsers}>Get users</button>
+      </div>
+    );
+  }
+
   return (
     <div className='App'>
-      <LoginForm />
+      <h1>
+        {store.isAuth ? `User ${store.user.email} is authorized` : "Log in"}
+      </h1>
+      <h1>
+        {store.user.isActivated
+          ? `Account is activated`
+          : "Activate your account by mail"}
+      </h1>
+      <button onClick={() => store.logout()}>Logout</button>
+      <div>
+        <button onClick={getUsers}>Get Users</button>
+      </div>
+      {users.map((user) => (
+        <div key={user.email}>{user.email}</div>
+      ))}
     </div>
   );
 };
 
-export default App;
+export default observer(App);
--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,10 +1,14 @@
 import { IUser } from "../models/IUser";
 import { makeAutoObservable } from "mobx";
 import AuthService from "../services/AuthService";
+import axios from "axios";
+import { AuthResponse } from "../models/response/AuthResponse";
+import { API_URL } from "../http";
 
 export default class Store {
   user = {} as IUser;
   isAuth = false;
+  isLoading = false;
 
   constructor() {
     makeAutoObservable(this);
@@ -16,6 +20,10 @@ export default class Store {
 
   setUser(user: IUser) {
     this.user = user;
+  }
+
+  setLoading(bool: boolean) {
+    this.isLoading = bool;
   }
 
   async login(email: string, password: string) {
@@ -51,6 +59,23 @@ export default class Store {
       this.setUser({} as IUser);
     } catch (e: any) {
       console.log(e.response?.data?.message);
+    }
+  }
+
+  async checkAuth() {
+    this.setLoading(true);
+    try {
+      const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {
+        withCredentials: true,
+      });
+      console.log(response);
+      localStorage.setItem("token", response.data.accessToken);
+      this.setAuth(true);
+      this.setUser(response.data.user);
+    } catch (e: any) {
+      console.log(e.response?.data?.message);
+    } finally {
+      this.setLoading(false);
     }
   }
 }
